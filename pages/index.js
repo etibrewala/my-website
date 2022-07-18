9b5a2fb491069a6f129d61d5281b3ec1685a01f9
Eshaan Tibrewala <eshaantibrewala29@gmail.com>
2022-07-17 21:21:21 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+<div>
+  <img/>
+  <div>
+    <h1>Eshaan Tibrewala</h1>
+    <div>
+      <a href='https://github.com/etibrewala' target="_blank"><div>github</div></a>
+      <div>email</div>
+      <a href="https://www.linkedin.com/in/eshaantibrewala" target="_blank">
+        <div>linkedin</div>
+      </a>
+      <div>resume</div>
+    </div>
+  </div>
+</div>
+  )
+}
