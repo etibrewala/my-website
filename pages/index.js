eaaac651e54debab9c639017f2c7399a4e98683a
Eshaan Tibrewala <eshaantibrewala29@gmail.com>
2022-07-18 18:36:06 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,16 @@ export default function Home() {
   <Head>
     <title>eshaan</title>
   </Head>
-  <div className="topnav">
+  {/*<div className="topnav">
   <a className="active" href="#home">Home</a>
   <a href="#news">News</a>
   <a href="#contact">Contact</a>
   <a href="#about">About</a>
-</div>
+  </div>*/}
   <div className={styles.center}>
     <h2 className={styles.fonts}>eshaan tibrewala</h2>
     </div>
-    <a href='https://www.linkedin.com/in/eshaantibrewala/' target="_blank"><div>linkedin</div></a>
-    <a href='https://github.com/etibrewala' target="_blank"><div>github</div></a>
+  <a href='https://www.linkedin.com/in/eshaantibrewala/' target="_blank">linkedin</a>
     <div>email</div>
   </div>
   )
