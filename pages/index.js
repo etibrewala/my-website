4d407bc0be61cc7d7eb687f4f9d722c01da1ad33
Eshaan Tibrewala <eshaantibrewala29@gmail.com>
2022-07-17 23:19:19 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,21 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-<div>
+<div className={styles.bakers}>
   <img/>
-  <div>
-    <h1>Eshaan Tibrewala</h1>
-    <div>
+  <div className={styles.center}>
+    <h2>Eshaan Tibrewala</h2>
+    </div>
+   
+   
+   {/*<div>
       <a href='https://github.com/etibrewala' target="_blank"><div>github</div></a>
       <div>email</div>
       <a href="https://www.linkedin.com/in/eshaantibrewala" target="_blank">
         <div>linkedin</div>
       </a>
-      <div>resume</div>
-    </div>
+      <div>resumes</div>
+  </div>*/}
   </div>
-</div>
   )
 }
