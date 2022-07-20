f832fb365e665a97e4205e2e599a914f72f1cd43
Eshaan Tibrewala <eshaantibrewala29@gmail.com>
2022-07-20 00:15:07 -0500

    practice

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,32 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+{/*import {
+  FaLinkedinIn,
+  FaGithub,
+  FaSpotify,
+} from "react-icons/fa";*/}
+
+
+
 export default function Home() {
   return (
 <div>
   <Head>
     <title>eshaan</title>
   </Head>
-  {/*<div className="topnav">
-  <a className="active" href="#home">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-  </div>*/}
+
   <div className={styles.center}>
     <h2 className={styles.fonts}>eshaan tibrewala</h2>
     </div>
+  <div>
   <a href='https://www.linkedin.com/in/eshaantibrewala/' target="_blank" rel="noreferrer">linkedin</a>
-  <a href='https://github.com/etibrewala' target="_blank" rel="noreferrer">github</a>
-    <div>email</div>
+    </div>
+    <div>
+    <a href='https://github.com/etibrewala' target="_blank" rel="noreferrer">github</a>
+    </div>
+    <div>
+      <a href="mailto: eshaantibrewala@gmail.com">email</a></div>
   </div>
   )
 }
