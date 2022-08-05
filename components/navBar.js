d7e14a3adcde413cf8243b3cde6b45b3f021820c
Eshaan Tibrewala <eshaantibrewala29@gmail.com>
2022-08-05 16:44:05 -0500

    updating nav bar

--- components/navBar.js
+++ components/navBar.js
@@ -1,14 +1,36 @@
+import Head from 'next/head';
 import Link from 'next/link'
+import styles from '../styles/NavBar.module.css'
+
 
 const NavBar = () => {
     return(
-        <div>
+        <main>
+        <Head>
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova:wght@500&family=Raleway&display=swap');
+            </style>
+        </Head>
+        <div className={styles.navbar}>
+        <div className={styles.mainlink}>
             <Link href="/"><a>eshaan</a></Link>
-            <Link href="/about"><a>about</a></Link>
-            <Link href="/resume"><a>resume</a></Link>
-            <Link href="/projects"><a>projects</a></Link>
-            <Link href="/photos"><a>photos</a></Link>
         </div>
+        <div className={styles.links}>
+            <div>
+            <Link href="/about"><a>about</a></Link>    
+            </div>
+            <div>
+            <Link href="/resume"><a>resume</a></Link>    
+            </div>
+            <div>
+            <Link href="/projects"><a>projects</a></Link> 
+            </div>
+            <div>
+            <Link href="/photos"><a>photos</a></Link>  
+            </div>
+        </div>
+        </div>
+        </main>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ import NavBar from '../components/navBar'
 
 export default function Home() {
 
-  const gh = "gh"
   return (
   <main>
     <div>
@@ -23,25 +22,7 @@ export default function Home() {
       <a href='https://github.com/etibrewala' target="_blank" rel="noreferrer">github</a>
       <a href="mailto:eshaantibrewala@gmail.com">email</a>
     </div>
-    
-{/*
-  <div className={styles.bubbleButtons}></div>
- <div className={styles.bubbles}>
-  <div className={styles.gh}>
-    <a href='https://www.linkedin.com/in/eshaantibrewala/' target="_blank">{gh}</a>    
-  </div>
-  
- </div>
- 
-  <div>
-    <a href='https://github.com/etibrewala' target="_blank" rel="noreferrer">github</a>
-  </div>
-  <div>
-    <a href="mailto:eshaantibrewala@gmail.com">email</a>
-  </div>
-  <div className={styles.bubbles}></div>
-  */}
-    
   </main>
   )
+  
 }
